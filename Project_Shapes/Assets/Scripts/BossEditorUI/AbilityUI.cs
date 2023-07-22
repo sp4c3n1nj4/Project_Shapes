bf403f657dc5d083eaf0ff0d96e1795a0f2df34a
sp4c3n1nj4 <73110602+sp4c3n1nj4@users.noreply.github.com>
2023-07-22 05:22:14 +0200

    UI 0.6

--- Project_Shapes/Assets/Scripts/BossEditorUI/AbilityUI.cs
+++ Project_Shapes/Assets/Scripts/BossEditorUI/AbilityUI.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AbilityUI : MonoBehaviour
 {
@@ -12,8 +15,27 @@ public class AbilityUI : MonoBehaviour
     [SerializeField]
     private GameObject[] moveUI, attackUI;
 
-    private BossAttack bossAttack;
+    [SerializeField]
     private BossMove bossMove;
+    [SerializeField]
+    private TMP_InputField moveTime, movePositionX, movePositionY;
+    [SerializeField]
+    private Slider moveSpeed;
+    [SerializeField]
+    private TMP_Dropdown moveTarget;
+
+    [SerializeField]
+    private BossAttack bossAttack;
+    [SerializeField]
+    private TMP_InputField attackTime, attackCastTime, attackScaleX, attackScaleY, attackPositionX, attackPositionY, attackDamage, attackDuration,
+        attackKnockbackX, attackKnockbackY, attackDDDuration, attackSlowDuration;
+    [SerializeField]
+    private Slider attackRotation;
+    [SerializeField]
+    private TMP_Dropdown attackShape, attackTarget;
+    [SerializeField]
+    private Toggle attackEffectDamage, attackEffectContinous, attackEffectKnockback, attackStatusDD, attackStatusSlow;
+
 
     public void UpdateEntry(int index)
     {
@@ -29,9 +51,52 @@ public class AbilityUI : MonoBehaviour
         }
     }
 
+    private void DisplayMoveEdit()
+    {
+        ToggleUIMode(false);
+        PopulateMoveUI();
+
+        print("Move:");
+
+        BindingFlags bindingFlags = BindingFlags.Public |
+                            BindingFlags.NonPublic |
+                            BindingFlags.Instance |
+                            BindingFlags.Static;
+        foreach (FieldInfo field in bossMove.GetType().GetFields(bindingFlags))
+        {
+            Debug.Log(field.GetValue(bossMove));
+        }
+    }
+
+    private void PopulateMoveUI()
+    {
+        moveTime.text = bossMove.time.ToString();
+        moveSpeed.value = bossMove.moveSpeed;
+
+        int i = 0;
+        switch (bossMove.target)
+        {
+            case BossAbilityTarget.none:
+                i = 0;
+                break;
+            case BossAbilityTarget.boss:
+                i = 1;
+                break;
+            case BossAbilityTarget.player:
+                i = 2;
+                break;
+        }
+        moveTarget.value = i;
+
+        movePositionX.text = bossMove.offSet[0].x.ToString();
+        movePositionY.text = bossMove.offSet[0].y.ToString();
+    }
+
     private void DisplayAttackEdit()
     {
         ToggleUIMode(true);
+        PopulateAttackUI();
+
         print("Attack:");
 
         BindingFlags bindingFlags = BindingFlags.Public |
@@ -44,19 +109,57 @@ public class AbilityUI : MonoBehaviour
         }
     }
 
-    private void DisplayMoveEdit()
+    private void PopulateAttackUI()
     {
-        ToggleUIMode(false);
-        print("Move:");
-
-        BindingFlags bindingFlags = BindingFlags.Public |
-                            BindingFlags.NonPublic |
-                            BindingFlags.Instance |
-                            BindingFlags.Static;
-        foreach (FieldInfo field in bossMove.GetType().GetFields(BindingFlags.Public))
+        attackTime.text = bossAttack.time.ToString();
+        attackCastTime.text = bossAttack.boxAttacks[0].castTime.ToString();
+        int shape = 0;
+        switch (bossAttack.boxAttacks[0].hitBoxType)
         {
-            Debug.Log(field.GetValue(bossMove));
+            case HitBoxType.Cylinder:
+                shape = 1;
+                break;
+            case HitBoxType.Donut:
+                shape = 2;
+                break;
         }
+        attackShape.value = shape;
+        attackRotation.value = bossAttack.boxAttacks[0].Yrotation;
+        attackScaleX.text = bossAttack.boxAttacks[0].scale.x.ToString();
+        attackScaleY.text = bossAttack.boxAttacks[0].scale.y.ToString();
+
+        int target = 0;
+        switch (bossAttack.boxAttacks[0].target)
+        {
+            case BossAbilityTarget.boss:
+                target = 1;
+                break;
+            case BossAbilityTarget.player:
+                target = 2;
+                break;
+        }
+        attackTarget.value = target;
+        attackPositionX.text = bossAttack.boxAttacks[0].offSet[0].x.ToString();
+        attackPositionY.text = bossAttack.boxAttacks[0].offSet[0].y.ToString();
+
+        if (bossAttack.boxAttacks[0].effect.Contains(HitBoxEffect.damage))
+        {
+            attackEffectDamage.isOn = true;
+            attackDamage.text = bossAttack.boxAttacks[0].damage.ToString();
+        }
+        if (bossAttack.boxAttacks[0].effect.Contains(HitBoxEffect.continous))
+        {
+            attackEffectContinous.isOn = true;
+            attackDuration.text = bossAttack.boxAttacks[0].hitBoxDuration.ToString();
+        }
+        if (bossAttack.boxAttacks[0].effect.Contains(HitBoxEffect.knockback))
+        {
+            attackEffectKnockback.isOn = true;
+            attackKnockbackX.text = bossAttack.boxAttacks[0].direction.x.ToString();
+            attackKnockbackY.text = bossAttack.boxAttacks[0].direction.y.ToString();
+        }
+
+        
     }
 
     private void ToggleUIMode(bool isAttack)
